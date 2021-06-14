0f21a4a1678df3da8eee1fe96cbe14eb142b73df
kosta <kostakostyukov@gmail.com>
2021-06-14 10:07:14 +0300

    Basic class for branches create + added base db connection

---
+++ MusicIsUs/Models/Branches.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication7.Models
+{
+    public class Branches
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public int Street { get; set; }
+    }
+}
