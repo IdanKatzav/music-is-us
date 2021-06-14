bfa758d5bcf26d9ac9c138bef7ff56528df8e751
kosta <kostakostyukov@gmail.com>
2021-06-14 11:02:38 +0300

    Added the basic logic for displaying branch list in the contactUs Page

--- MusicIsUs/Controllers/HomeController.cs
+++ MusicIsUs/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication7.Models;
 
 namespace MusicIsUs.Controllers
 {
     public class HomeController : Controller
     {
+        private MusicIsUsContext dbConn = new MusicIsUsContext();
         public ActionResult Index()
         {
             return View();
@@ -23,8 +25,10 @@ namespace MusicIsUs.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+            ViewBag.listOfBranchCities = dbConn.Branches.Select(b => b.City).Distinct();
+            ViewBag.listOfBranchStreets = dbConn.Branches.Select(b => b.Street);
 
-            return View();
+            return View(dbConn.Branches.ToList()) ;
         }
     }
 }
