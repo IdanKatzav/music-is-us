0973c430fa24bb8aacc518384d2c88e76923acd7
kosta <kostakostyukov@gmail.com>
2021-06-15 14:15:51 +0300

    Added google map API to the contact page and created js file for supporting looking up the branch address on the map

---
+++ MusicIsUs/Scripts/MapHandler.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function getBranchAddress(){
+    $('.displayOnMap').click(function () {
+        var prop = $(this).data('prop');
+        parseAddressForMapAPI(prop)
+    })
+    function parseAddressForMapAPI(address) {
+        console.log(address);
+        var parsedBySymbol = address.split(", ");
+        var tempStr = [parsedBySymbol.length];
+        var parsedAddress = "";
+        for (var i = 0; i < parsedBySymbol.length;i++) {
+            tempStr[i] = parsedBySymbol[i].replaceAll(" ", "+");
+            parsedAddress += tempStr[i] + ",";
+        }
+        setSrcForMap(parsedAddress);
+    }
+    function setSrcForMap(AddressToDisplay) {
+        $(".mapDisplay").attr('src', "https://www.google.com/maps/embed/v1/place?key=AIzaSyBsG-gcX20RziyqnfWo6p-H44M4vbECMQ4&q=" + AddressToDisplay);
+    }
+})
