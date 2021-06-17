03ac63da47d956e7b201680438834915921f982c
kosta <kostakostyukov@gmail.com>
2021-06-17 14:28:11 +0300

    conflict resolve

--- MusicIsUs/Controllers/HomeController.cs
+++ MusicIsUs/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using WebApplication7.Models;
+using MusicIsUs.Models;
 
 namespace MusicIsUs.Controllers
 {
