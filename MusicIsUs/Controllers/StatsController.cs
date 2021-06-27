e5ecfab74bedbdcadd11bc5fd9e419e6d03d1950
kosta <kostakostyukov@gmail.com>
2021-06-27 19:28:19 +0300

    started the stats page

---
+++ MusicIsUs/Controllers/StatsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicIsUs.Models;
+using Newtonsoft.Json;
+
+namespace MusicIsUs.Controllers
+{
+    public class StatsController : Controller
+    {
+        private MusicIsUsContext db = new MusicIsUsContext();
+        // GET: Stats
+        public ActionResult Index()
+        {
+            var groupMusicByGenre = db.Vinyls.Select(g => new { genre = g.Genere })
+                .GroupBy(r => r.genre)
+                .Select(i => new { genre = i.Key, count = i.Count() })
+                .ToList();
+            var groupMusicByGenreJson = JsonConvert.SerializeObject(groupMusicByGenre);
+            ViewBag.musicGenreJson = groupMusicByGenreJson;
+            return View();
+        }
+    }
+}
