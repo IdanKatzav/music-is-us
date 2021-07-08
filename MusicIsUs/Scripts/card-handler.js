435d38491bd3d640daca8834ef9fae8ba002eeee
IdanKatzav <idan.katzav@gmail.com>
2021-07-08 20:32:53 +0300

    fix: add card-handler.js

---
+++ MusicIsUs/Scripts/card-handler.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    $("#instLink").click(function () {
+        if (!($("#instLink").hasClass("active"))) {
+            $("#instLink").addClass("active");
+            $("#vinLink").removeClass("active");
+            $("#recommVinyls").addClass("hide");
+            $("#recommInstruments").removeClass("hide");
+        }
+    });
+
+    $("#vinLink").click(function () {
+        if (!($("#vinLink").hasClass("active"))) {
+            $("#vinLink").addClass("active");
+            $("#instLink").removeClass("active");
+            $("#recommInstruments").addClass("hide");
+            $("#recommVinyls").removeClass("hide");
+        }
+    });
+})
+
